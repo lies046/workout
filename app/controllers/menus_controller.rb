d9ef8df743c90e397e7c95436916155494fcaaab
lies046 <skytree.skysky@gmail.com>
2020-04-04 16:42:01 +0900

    Add MenuController and Model

---
+++ app/controllers/menus_controller.rb
@@ -0,0 +1,27 @@
+class MenusController < ApplicationController
+  def index
+  end
+
+  def show
+    @menu = Menu.find(params[:id])
+  end
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    @menu.user_id = current_user.id
+    @menu.save
+    redirect_to menus_path(@menu)
+  end
+
+  def edit
+  end
+
+  private
+  def menu_params
+    params.require(:menu).permit(:title, :body, :image)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_052027) do
+ActiveRecord::Schema.define(version: 2020_04_04_071817) do
+
+  create_table "menus", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
