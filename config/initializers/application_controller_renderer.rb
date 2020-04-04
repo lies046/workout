69afcb949f4fa8149798ca79b22fff2c59b099a2
lies046 <skytree.skysky@gmail.com>
2020-04-04 16:16:54 +0900

    Modified UsersEdit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'acb4b23174aa39b99d53b824e44c5db2185e61c89ae45f9c9ff9d6f1458f445d88cdebcc3ca979cb39e6b73fcccd1f2cbf987dc4c33c560b031cf1c3efec0b1e'
